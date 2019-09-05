bc3ac2409155b7328d4d7eb35fa589f59fc564d1
LFA <javalicarmim@gmail.com>
2019-09-05 16:11:23 -0300

    App Update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './App.sass';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />    
+  );
+}
+
+export default App;
