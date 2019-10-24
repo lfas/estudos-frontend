d0140edd474c593197fde36b963fa67a79745aa3
LFA <javalicarmim@gmail.com>
2019-10-24 10:50:16 -0300

    New Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -31,7 +31,7 @@ export default function Main({ match }) {
         }
 			});
 			
-			response.data.sort((a, b) => a.name.localeCompare(b.name));
+			//response.data.sort((a, b) => a.name.localeCompare(b.name));
 
 			setSubjects(response.data);
 		};
@@ -101,7 +101,7 @@ export default function Main({ match }) {
                   Olá, {user.name}
                 </h1>
                 <h2 className="subtitle has-text-link is-hidden">
-                  Você concluiu 10 de 30 Atividades 
+                  Você concluiu 0 de 0 Exercícios 
                 </h2>
               </div>
             </div>
@@ -109,48 +109,32 @@ export default function Main({ match }) {
         </div>
       </section>
 			
-      <section className="container p-t-xl p-b-xl">
-        <div className="block p-sm">
-          {subjects.map(subject => (
-            <div key={subject._id}>
-              <h1 className="title">{subject.name}</h1>
-								{subject.books.map(book => (
-									<div className="subject-block" key={book._id}>
-										<h2 className="subtitle is-5">Apostila {book.name}</h2>
-											{book.themes.map(theme =>(
-												<React.Fragment key={theme._id}>
-													<h2 className="title is-4">{theme.name}</h2>
-													<div className="columns is-multiline">
-														{theme.activities.map(activity => (	
-															<div className="column is-4" key={activity._id}>
-																<p>Aula {activity.class_number} (p. {activity.page})</p>															
-																<label className="checkbox p-r-md">
-																	<input type="checkbox" onClick={() => handleActivity(activity._id, 'ad', activity.ad)} defaultChecked={activity.ad} />															
-																	AD
-																</label>
-																<label className="checkbox p-r-md">
-																	<input type="checkbox" onClick={() => handleActivity(activity._id, 'tm', activity.tm)} defaultChecked={activity.tm} />
-																	TM
-																</label>
-																<label className="checkbox p-r-md">
-																	<input type="checkbox" onClick={() => handleActivity(activity._id, 'tc', activity.tc)} defaultChecked={activity.tc} />
-																	TC
-																</label>
-																<label className="checkbox p-r-md">
-																	<input type="checkbox" onClick={() => handleActivity(activity._id, 'td', activity.td)} defaultChecked={activity.td} />
-																	TD
-																</label>
-															</div>
-														))}
-													</div>
-												</React.Fragment>
-											))}
-										<hr/>
+      <section className="container p-t-lg p-b-xl p-l-sm p-r-sm">
+				{subjects.map(subject => (
+					<React.Fragment key={subject._id}>
+						<h1 className="title">{subject.name}</h1>
+						
+						<div className="block is-transition">
+							<div className="columns is-multiline">
+								{subject.activities.map(activity => (
+									<div className="column is-3" key={activity._id}>
+										<h2 className={"title is-4 is-marginless " + (activity.es && activity.ec ? 'is-completed' : '')}>{activity.theme}</h2>
+
+										<label className={"checkbox p-r-md " + (activity.es ? 'is-completed' : '')}>
+											<input type="checkbox" className="m-l-xs m-r-xs" onClick={() => handleActivity(activity._id, 'es', activity.es)} defaultChecked={activity.es} />															
+											Sala
+										</label>
+										<label className={"checkbox p-r-md " + (activity.ec ? 'is-completed' : '')}>
+											<input type="checkbox" className="m-l-xs m-r-xs" onClick={() => handleActivity(activity._id, 'ec', activity.ec)} defaultChecked={activity.ec} />
+											Complementar
+										</label>
 									</div>
 								))}
-            </div>
-          ))}
-        </div>
+							</div>
+							<hr />
+						</div>
+					</React.Fragment>
+				))}
       </section>
     </>
   );
